f8b3e73a811bd20412bfd9e17d19ec40ba6186a4
Bronwyn Gautier <bronwyn.gautier@student.adelaide.edu.au>
2025-06-20 07:12:01 +0000

    [automatic commit] 2025-06-20 07:12:01 UTC 129.127.248.67

--- part1/public/javascripts/indexjs.js
+++ part1/public/javascripts/indexjs.js
@@ -6,34 +6,6 @@ const TOP = [
     { title:'Contact Us',   url:'/contact' }
     ];
 
-    /*
-    Top menu 2 (task 1.7):
-
-    { title:'Home', url:'/', submenus: [] }
-    { title:'About', url:'/about',
-        submenus: [
-            { title:'Who we are',   url:'/about#us' },
-            { title:'What we do',   url:'/about#store' },
-            { title:'Our range',     url:'/about#range' }
-        ]
-    }
-    { title:'Contact Us',   url:'/contact',
-        submenus: [
-            { title:'Information',   url:'/contact#info' },
-            { title:'Returns',   url:'/contact#return' },
-            { title:'Locate Us',     url:'/contact#locate' }
-        ]
-    }
-
-
-    Stores (task 1.8):
-
-    { name:'Adelaide City',  address:'45 Florabunda Lane, Adelaide, 5000', counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg/320px-11_Gloddaeth_Street%2C_Llandudno_shop_front.jpg' },
-    { name:'Steelton South', address:'77 Weigall Avenue, Steelton, 5413',  counter: 0, img:'https://upload.wikimedia.org/wikipedia/commons/4/42/Well-shop-front.jpg' },
-    { name:'Milton',         address:'33 McGregor Street, Milton, 5880',   counter: 0, img:'https://upload.wikimedia.org/wikipedia/en/thumb/e/e2/Greggs_store_front.JPG/320px-Greggs_store_front.JPG' }
-
-    */
-
     const SPECIALS = [
         { name: 'Salt', price: '$0.99', url: 'https://upload.wikimedia.org/wikipedia/commons/7/7e/Korean_sea_salt.jpg' },
         { name: 'Pepper', price: '$2.49', url: 'https://live.staticflickr.com/191/449547916_ce1c87adeb_b.jpg' },
