41db2b1a724f0ca996a4b959670f9bd974c4bb48
Bronwyn Gautier <bronwyn.gautier@student.adelaide.edu.au>
2025-06-20 04:38:59 +0000

    [automatic commit] 2025-06-20 04:38:59 UTC 129.127.248.67

--- part1/app.js
+++ part1/app.js
@@ -58,7 +58,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
   // Route to return walker summaries as JSON
 app.get('/api/walkrequests/open', async (req, res) => {
     try {
-      const [walkersummaries] = await db.execute('SELECT Users.username, COUNT(WalkRatings.rating) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRatings.rating_id) AS completed_walks FROM WalkRatings INNER JOIN Users ON WalkRatings.walker_id = Users.user_id WHERE WalkRatings.walker_id;');
+      const [walkersummaries] = await db.execute('SELECT Users.username, COUNT(WalkRatings.rating) AS total_ratings, AVG(WalkRatings.rating) AS average_rating, COUNT(WalkRatings.rating_id) AS completed_walks FROM WalkRatings INNER JOIN Users ON WalkRatings.walker_id = Users.user_id WHERE WalkRatings.walker_id = 2;');
       res.json(walkersummaries);
     } catch (err) {
       res.status(500).json({ error: 'Failed to fetch walker summaries' });
