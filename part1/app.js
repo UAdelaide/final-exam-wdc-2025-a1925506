9b8d76ba0f9fc59ce49cc6fc0e1a9d4bb64d929d
Bronwyn Gautier <bronwyn.gautier@student.adelaide.edu.au>
2025-06-20 02:47:47 +0000

    [automatic commit] 2025-06-20 02:47:47 UTC 129.127.248.67

--- part1/app.js
+++ part1/app.js
@@ -35,7 +35,7 @@ let db;
   }
 })();
 
-// Route to return books as JSON
+// Route to return dog info as JSON
 app.get('/api/dogs', async (req, res) => {
   try {
     const [dogInfo] = await db.execute('SELECT Dogs.name, Dogs.size, Users.username FROM Dogs INNER JOIN Users ON Dogs.owner_id = Users.user_id;');
