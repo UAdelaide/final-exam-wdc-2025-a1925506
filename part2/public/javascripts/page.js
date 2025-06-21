445952a0ab7a4b8cd0a4fd27d25ebea2570dbd9f
Bronwyn Gautier <bronwyn.gautier@student.adelaide.edu.au>
2025-06-21 02:54:23 +0000

    [automatic commit] 2025-06-21 02:54:23 UTC 129.127.248.74

--- part2/public/javascripts/page.js
+++ part2/public/javascripts/page.js
@@ -188,7 +188,7 @@ function login(){
     // Define function to run on response
     xmlhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            alert("Welcome "+ this.response.user.user);
+            alert("Welcome "+this.re);
         } else if (this.readyState == 4 && this.status >= 400) {
             alert("Login failed");
         }
