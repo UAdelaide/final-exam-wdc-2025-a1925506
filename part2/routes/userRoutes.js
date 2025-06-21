06e2a765cfad0e57823ce79241417ad3d4b8f45b
Bronwyn Gautier <bronwyn.gautier@student.adelaide.edu.au>
2025-06-21 02:59:59 +0000

    [automatic commit] 2025-06-21 02:59:59 UTC 129.127.248.74

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -42,7 +42,7 @@ router.post('/login', async (req, res) => {
 
   try {
     const [rows] = await db.query(`
-      SELECT user_id, email, role FROM Users
+      SELECT user_id, username, role FROM Users
       WHERE username = ? AND password_hash = ?
     `, [user, pass]);
 
