69605ff09870e340152aff17d92d517eb90a7947
Bronwyn Gautier <bronwyn.gautier@student.adelaide.edu.au>
2025-06-21 01:13:52 +0000

    [automatic commit] 2025-06-21 01:13:52 UTC 129.127.248.74

--- part1/public/javascripts/page.js
+++ part1/public/javascripts/page.js
@@ -12,16 +12,30 @@
     createApp({
       data() {
         return {
-          dogImage: ''
+          dogImage: '',
+          clickCount: 0,
+          clicksRequired: 3 // how many times the button must be clicked
         };
       },
       async created() {
-        try {
-          const res = await fetch('https://dog.ceo/api/breeds/image/random',{method:'GET'});
-          const data = await res.json();
-          this.dogImage = data.message;
-        } catch (err) {
-          console.error('Failed to load dog image:', err);
+        this.loadDog(); // initial dog
+      },
+      methods: {
+        async loadDog() {
+          this.clickCount++;
+          if (this.clickCount < this.clicksRequired) {
+            console.log(`Clicked ${this.clickCount} time(s). Need ${this.clicksRequired - this.clickCount} more.`);
+            return;
+          }
+
+          try {
+            const res = await fetch('https://dog.ceo/api/breeds/image/random');
+            const data = await res.json();
+            this.dogImage = data.message;
+            this.clickCount = 0; // reset after successful load
+          } catch (err) {
+            console.error('Failed to load dog image:', err);
+          }
         }
       }
     }).mount('#app');
